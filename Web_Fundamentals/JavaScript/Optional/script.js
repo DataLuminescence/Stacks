fd867894cb4a42bdf97698ef91574d88fd382df1
Leonardo Robles <leonardojrobles@gmail.com>
2022-02-28 13:35:04 -0800

    update

--- Web_Fundamentals/JavaScript/Optional/script.js
+++ Web_Fundamentals/JavaScript/Optional/script.js
@@ -1,7 +1,16 @@
 console.log("Hello World");
 
-function terminal(){
-    for(var i=0; i<2; i++){
-
+function print255(){
+    for(var i=1; i<2; i++){
+        console.log(i);
     }
+}
+
+function dostuff(e){
+    console.log(e);
+//element.remove();
+//element.innerText = "This has changed!";
+
+    element.innerText = "<h1 class='important'><h1>";
+    element.innerHTML = "<button onclick='console.log(`more stuff`)'>Dynamic Button</button>";
 }
