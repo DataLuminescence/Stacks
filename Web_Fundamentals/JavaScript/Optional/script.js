bcce934c094618d3f3d4525fc8fcdd91a5ceeb44
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-01 13:55:04 -0800

    further testing

--- Web_Fundamentals/JavaScript/Optional/script.js
+++ Web_Fundamentals/JavaScript/Optional/script.js
@@ -1,16 +1,29 @@
 console.log("Hello World");
 
-function print255(){
-    for(var i=1; i<2; i++){
-        console.log(i);
-    }
-}
+// function print255(){
+//     for(var i=1; i<2; i++){
+//         console.log(i);
+//     }
+// }
 
-function dostuff(e){
-    console.log(e);
-//element.remove();
-//element.innerText = "This has changed!";
+// function dostuff(e){
+//     console.log(e);
+// //element.remove();
+// //element.innerText = "This has changed!";
 
-    element.innerText = "<h1 class='important'><h1>";
-    element.innerHTML = "<button onclick='console.log(`more stuff`)'>Dynamic Button</button>";
+//     element.innerText = "<h1 class='important'><h1>";
+//     element.innerHTML = "<button onclick='console.log(`more stuff`)'>Dynamic Button</button>";
+// }
+
+
+function d6() {
+    var roll = (Math.random()* (6 - 1 + 1) + 1);
+    return roll;
 }
+const element = document.getElementById("randomhelp");
+element.innerHTML = "" +roll;
+
+parseInt(element.innerHTML
+
+var playerRoll = d6();
+console.log("The player rolled: " + playerRoll);
