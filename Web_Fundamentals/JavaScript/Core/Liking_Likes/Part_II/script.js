c8fe21bc767b99b81299c88cb30fd95a1bdf19a5
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-03 20:25:41 -0800

    final commit

--- Web_Fundamentals/JavaScript/Core/Liking_Likes/Part_II/script.js
+++ Web_Fundamentals/JavaScript/Core/Liking_Likes/Part_II/script.js
@@ -1,17 +1,24 @@
-function buttonRename(element){
-    if(element.innerHTML == "Logout"){
-        element.innerHTML = "Login";
-    }else{
-        element.innerHTML = "Logout";
+
+//Can I get the values of the first 2 characters of 
+//string and parce into count1-3?
+
+var count1 = 9;
+var count2 = 12;
+var count3 = 9;
+
+function likeCounter(id){
+
+var countElement = document.querySelector("#"+id);
+console.log(countElement);
+
+    if(id == 'count1'){
+        count1++;
+        countElement.innerText = count1 + " likes";
+    }else if(id == 'count2'){
+        count2++;
+        countElement.innerText = count2 + " likes";
+    }else if(id == 'count3'){
+        count3++;
+        countElement.innerText = count3 + " likes";
     }
 }
-
-function likeCounter(element){
-    element.innerHTML =  parseInt(element.innerHTML) + 1 + " Likes";
-    alert("Ninja was liked " + element.innerHTML + " times!");
-}
-
-function buttonDelete(element){
-    element.style = "display: none;"
-}
-
