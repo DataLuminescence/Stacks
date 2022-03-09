e97a04fde1389eac1d96892a31b71d9f6a73dd89
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-09 00:31:14 -0800

    final commit

--- Python/Fundamentals/Fundamentals/Practice/Function_Basics_II/function_basic_II.py
+++ Python/Fundamentals/Fundamentals/Practice/Function_Basics_II/function_basic_II.py
@@ -1,45 +1,45 @@
 
-# def countdown(num):
-#     num_list = []
-#     for x in range(num,-1,-1):
-#         num_list.append(x)
-#     return num_list
+def countdown(num):
+    num_list = []
+    for x in range(num,-1,-1):
+        num_list.append(x)
+    return num_list
 
-# a = countdown(5)
-# print(a)
-
-
-# def print_and_return(list):
-#     print(list[0])
-#     return list[1]
-
-# b = print_and_return([1,2])
-# print(b)
+a = countdown(5)
+print(a)
 
 
-# def first_plus_length(list):
-#     sum = list[0]
-#     sum += len(list)
-#     return sum
+def print_and_return(list):
+    print(list[0])
+    return list[1]
 
-# c = first_plus_length([1,2,3,4,5])
-# print(c)
+b = print_and_return([1,2])
+print(b)
 
 
-# def  values_greater_than_second(list):
-#     if len(list) < 2:
-#         return False   
-#     list2 = []
-#     for x in range(0, len(list)):
-#         if list[x] > list[1]:
-#             list2.append(list[x])
-#     print(len(list2))
-#     return list2
+def first_plus_length(list):
+    sum = list[0]
+    sum += len(list)
+    return sum
 
-# d =  values_greater_than_second([5,2,3,2,1,4])
-# print(d)
-# e =  values_greater_than_second([3])
-# print(e)
+c = first_plus_length([1,2,3,4,5])
+print(c)
+
+
+def  values_greater_than_second(list):
+    if len(list) < 2:
+        return False   
+    list2 = []
+    for x in range(0, len(list)):
+        if list[x] > list[1]:
+            list2.append(list[x])
+    print(len(list2))
+    return list2
+
+d =  values_greater_than_second([5,2,3,2,1,4])
+print(d)
+e =  values_greater_than_second([3])
+print(e)
 
 def length_and_value(size,value):
     list3 = []
