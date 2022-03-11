f11ec1b0c882bb1eff36365246f41d69dd9ce749
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-10 20:00:50 -0800

    final commit

---
+++ Python/Fundamentals/OOP/Core/Users_With_Bank_Account/users_with_bank_accounts.py
@@ -0,0 +1,74 @@
+from optparse import check_builtin
+
+
+class BankAccount:
+    accounts = []
+    def __init__(self, interestRate, balance):
+        self.interestRate = interestRate #Attribute that each new bank instance will have
+        self.balance = balance #Attribute that each new bank instance will have
+        BankAccount.accounts.append(self) #Creates a record in accounts list of each new bank account created
+    
+    def deposit(self, amount):
+        #Points to itself because only account have a balance. We need user to point to account, then balance
+        self.balance += amount  
+        return self
+
+    def withdrawal(self, amount):
+        if(self.balance >= amount):
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        return(f"{self.balance}")
+
+    def yield_interest(self):
+        if(self.balance > 0):
+            self.balance += self.balance*self.interestRate
+        return self
+
+    @classmethod
+    def print_accounts(cls):
+        for account in cls.accounts:
+            account.display_account_info() 
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.account = {"checking" : BankAccount(interestRate=.02, balance=1000),
+                        "savings" : BankAccount(interestRate=.02, balance=800)
+        }
+
+    def make_deposit(self, amount):
+        self.account["checking"].deposit(amount)
+
+    def make_withdrawal(self, amount):
+        self.account["checking"].withdrawal(amount)
+
+    def display_user_balance(self):
+        print(f"User: {self.name}, Checking Balance: {self.account['checking'].display_account_info()}")
+        print(f"User: {self.name}, Savings Balance: {self.account['savings'].display_account_info()}")
+        return self
+
+    def transfer_money(self, user, amount):
+        self.account["checking"].withdrawal(amount)
+        user.account["checking"].withdrawal(amount)
+        self.account["checking"].display_account_info()
+        user.account["checking"].display_account_info()
+        return self
+
+guido = User("Guido")
+felix = User("Felix")
+
+guido.account['checking'].deposit(100)
+guido.account['savings'].deposit(100)
+
+felix.account['checking'].deposit(50)
+felix.account['savings'].deposit(50)
+
+guido.transfer_money(felix, 200)
+
+guido.display_user_balance()
+felix.display_user_balance()
