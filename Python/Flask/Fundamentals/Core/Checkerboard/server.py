0d07f8e6e90606149e60fe99d2524f7087f66f49
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-13 19:03:19 -0700

    final commit

--- Python/Flask/Fundamentals/Core/Checkerboard/server.py
+++ Python/Flask/Fundamentals/Core/Checkerboard/server.py
@@ -1,21 +1,22 @@
+import string
 from flask import Flask, render_template #import flask and allow us to create our application
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", x=4, y=4, color1="", color2="")
 
 @app.route('/4')
 def half():
-    return render_template("index.html", number=4)
+    return render_template("index.html", x=4, y=2, color1="", color2="")
 
-@app.route('/play/<int:box_num>')
-def dimensions(box_num):
-    return render_template("index.html", number=box_num, color='#9fc5f8' )
+@app.route('/<int:x>/<int:y>')
+def dimensions(x,y):
+    return render_template("index.html", x=int(x), y=int(y), color1="", color2="")
 
-@app.route('/play/<int:box_num>/<box_color>')
-def play_number_color(box_num, box_color):
-    return render_template("index.html", number=box_num, color=box_color)
+@app.route('/<int:x>/<int:y>/<string:color1>/<string:color2>')
+def shade(x,y,color1,color2):
+    return render_template("index.html", x=int(x), y=int(y), color1=color1, color2=color2)
 
 if __name__=="__main__":
     app.run(debug=True)
