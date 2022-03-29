78a78de0fb3e42f7b4cc041cba8e264f4317858b
Leonardo Robles <leonardojrobles@gmail.com>
2022-03-28 22:21:19 -0700

    update

--- Python/Flask_MySQL/Belt_Review/belt_exam/flask_app/controllers/band_controller.py
+++ Python/Flask_MySQL/Belt_Review/belt_exam/flask_app/controllers/band_controller.py
@@ -7,11 +7,13 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)  
 
 #------------------------------------------------------------------------------
-# NEW SIGHTING - Band creation area
+# GO TO NEW BAND PAGE - DISPLAY
 #------------------------------------------------------------------------------
 
-@app.route("/new/sighting") 
-def band_new():
+# routes user to band creation area
+@app.route("/band/new")
+def new_band():
+
     # validate user is logged in to enter this page
     if "user_id" not in session:
         return redirect("/")
@@ -19,47 +21,17 @@ def band_new():
         "id" : session["user_id"]
     }
 
+    #
     user = User.get_by_user_id(data)
 
     return render_template("band_new.html", user = user)
 
 #------------------------------------------------------------------------------
-# CREATE BAND
-#------------------------------------------------------------------------------
-
-# actually creates band after validation, and obtaining data from forms
-@app.route("/register_band", methods=['POST'])
-def register_band():
-
-    # if band submitted registration info is invalid return user to home and 
-    # display a message of where they made a mistake
-    if not Band.validate_register(request.form):
-        return redirect("/new/sighting")
-    
-    # create a dictionary from data collected from form
-    query_data = {
-        "user_id" : session["user_id"],
-        "band_name" : request.form["band_name"],
-        "genre" : request.form["genre"],
-        "homecity" : request.form["homecity"],
-    }
-
-    # insert dictionary data into create_band class method in order to populate 
-    # fields needed to insert new user into database. Also returns new user id
-    new_band_id = Band.create_band(query_data) 
-
-    # store user id into session
-    # session["user_id"] = new_band_id #Why cant we use this to use the user id?
-
-    # redirect elsewhere
-    return redirect("/dashboard")
-
-#------------------------------------------------------------------------------
-# EDIT BAND - ACTION
+# EDIT BAND - DISPLAY
 #------------------------------------------------------------------------------
 
 # takes user to band edit area
-@app.route("/edit/<int:id>")
+@app.route("/band/edit/<int:id>")
 def edit_band(id):
 
     # validate user is logged in to enter this page
@@ -77,9 +49,11 @@ def edit_band(id):
     user = User.get_by_user_id(user_data)
     print(user)
 
+    # get band by id we dont care about user EDIT 
     one_band = Band.get_user_and_band_by_band_id(band_data) #List of dictionaries
     print(one_band)
 
+    # EDIT do it the right way
     return render_template("band_edit.html", user = user, one_band = one_band)
 
 #------------------------------------------------------------------------------
@@ -87,8 +61,9 @@ def edit_band(id):
 #------------------------------------------------------------------------------
 
 # takes user to band edit area
-@app.route("/mybands")
-def view_band():
+@app.route("/band/mybands")
+def view_band(): # change name EDIT
+    
     # validate user is logged in to enter this page
     if "user_id" not in session:
         return redirect("/")
@@ -98,17 +73,73 @@ def view_band():
     }
 
     user = User.get_by_user_id(user_data)
-    one_band = Band.get_user_and_band_by_user_id(user_data)
-    print(one_band)
+    one_band = Band.get_user_and_band_by_user_id(user_data) #one band not appropriate name EDIT
+
     return render_template("user_band_view.html", user = user, one_band = one_band)
+
+#------------------------------------------------------------------------------
+# CREATE BAND - ACTION
+#------------------------------------------------------------------------------
+
+# creates band after validation, and obtaining data from forms
+@app.route("/band/register", methods=['POST'])
+def band_register():
+
+    # validate user is logged in to enter this page
+    if "user_id" not in session:
+        return redirect("/")
+    
+    # if band submitted registration info is invalid return user to home and 
+    # display a message of where they made a mistake
+    if not Band.validate_register(request.form):
+        return redirect("/band/new")
+    
+    # create a dictionary from data collected from form
+    query_data = {
+        "user_id" : session["user_id"],
+        "band_name" : request.form["band_name"],
+        "genre" : request.form["genre"],
+        "homecity" : request.form["homecity"]
+    }
+
+    # insert dictionary data into create_band class method in order to populate 
+    # fields needed to insert new user into database
+    Band.create_band(query_data) 
+
+    # redirect elsewhere
+    return redirect("/dashboard")
+
+
+#------------------------------------------------------------------------------
+# UPDATE BAND - ACTION
+#------------------------------------------------------------------------------
+
+@app.route("/band/update/<int:id>", methods=['POST'])
+def upate_band(id):
+    # validate user is logged in to enter this page
+    if "user_id" not in session:
+        return redirect("/")
+    
+    query_data = {
+        "id": id,
+        "user_id" : session["user_id"],
+        "band_name" : request.form["band_name"],
+        "genre" : request.form["genre"],
+        "homecity" : request.form["homecity"]
+    }
+    
+    Band.update_band_by_band_id(query_data)
+
+    return redirect("/dashboard")
 
 #------------------------------------------------------------------------------
 # DELETE BAND - ACTION
 #------------------------------------------------------------------------------
 
 # deletes a band based on the id of the band selected in dashboard
-@app.route("/delete_band/<int:id>")
+@app.route("/band/delete/<int:id>")
 def delete_band(id):
+    
     # validate user is logged in to enter this page
     if "user_id" not in session:
         return redirect("/")
