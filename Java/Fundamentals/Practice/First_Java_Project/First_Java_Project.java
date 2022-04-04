4038b98948ce7a32e356de102464910bff6fe489
Leonardo Robles <leonardojrobles@gmail.com>
2022-04-04 11:33:53 -0700

    folder change

---
+++ Java/Fundamentals/Practice/First_Java_Project/First_Java_Project.java
@@ -0,0 +1,9 @@
+public class First_Java_Project{
+
+    public static void main(String[] args){
+        String title = "First Java program";
+        System.out.println("My name is Leo Robles");
+        System.out.println("I am 35 years old");
+        System.out.println("My hometown is San Diego, CA");
+    }
+}
